ce63d68aa9008626fd2f5271ed8e43a5990279fa
Jac22 <jacopo.prudentino@gmail.com>
2023-05-16 12:27:50 +0200

    Created PhotoFormModel and an attribute to check if the file posted is a valid image

---
+++ net-il-mio-fotoalbum/Models/IsAnImgValidationAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace net_il_mio_fotoalbum.Models
+{
+    public class IsAnImgValidationAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object value, ValidationContext validationContext)
+        {
+            IFormFile img = (IFormFile)value;
+            int index = img.FileName.LastIndexOf('.');
+            string ext = img.FileName.Substring(index + 1);
+            if(ext == "jpg" ||  ext == "png" || ext == "svg" || ext == "webp" || ext == "jpeg")
+                return ValidationResult.Success;
+            return new ValidationResult("Formato del file caricato non valido. Formati validi: png, jpg, jpeg, webp, svg");
+        }
+    }
+}
---
+++ net-il-mio-fotoalbum/Models/PhotoFormModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace net_il_mio_fotoalbum.Models
+{
+    public class PhotoFormModel
+    {
+        [Required(ErrorMessage = "Campo obbligatorio!")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Il titolo deve essere compreso tra 2 e 50 caratteri")]
+        public string Title { get; set; }
+        [StringLength(250)]
+        public string? Description { get; set; }
+        [Required]
+        [IsAnImgValidation]
+        public IFormFile Img { get; set; }
+        [Required]
+        public bool visibile { get; set; }
+        public List<Category> AllCategories { get; set; }
+        public List<int> SelectedCategories { get; set; }
+    }
+}
