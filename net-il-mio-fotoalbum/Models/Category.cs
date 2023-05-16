cbdb41421298ec886a4f9e1868a2be905e1dab05
Jac22 <jacopo.prudentino@gmail.com>
2023-05-16 09:46:56 +0200

    Add project files.

---
+++ net-il-mio-fotoalbum/Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace net_il_mio_fotoalbum.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(150, MinimumLength = 5)]
+        public string Description { get; set; }
+        public List<Photo> Photos { get; set; }
+
+        public Category(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+    }
+}
---
+++ net-il-mio-fotoalbum/Models/Photo.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace net_il_mio_fotoalbum.Models
+{
+    public class Photo
+    {
+        [Key] 
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo obbligatorio!")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Il titolo deve essere compreso tra 2 e 50 caratteri")]
+        public string Title { get; set; }
+        [StringLength(250)]
+        public string? Description { get; set; }
+        [Required]
+        public string Img { get; set; }
+        [Required]
+        public bool visibile { get; set; }
+        public List<Category>? categories { get; set; }
+    }
+}
